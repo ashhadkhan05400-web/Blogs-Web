e8f2438af984ed42eec72e83c69f15d62b0414a4
ashhad khan <ashhadkhan05400@gmail.com>
2026-09-14 23:41:30 +0500

    Delete src directory

--- src/other_services/toastify.jsx
+++
@@ -1,8 +0,0 @@
-
-import { toast } from 'react-toastify';
-
-export default function toastify(message, type = "default") {
-  if (type === "error") toast.error(message);
-  else if (type === "success") toast.success(message);
-  else toast(message);
-}
